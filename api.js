72af2ccaf579838b37fa98a038e36292e075e87d
LavanyaBabu <lavanyababu709@gmail.com>
2023-11-01 00:17:49 +0530

    Altered parameters to student table

--- api.js
+++ api.js
@@ -14,12 +14,22 @@ router.get('/students', async (req, res) => {
   }
 });
 
+router.get('/students/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const allStudents = await pool.query('SELECT * FROM student WHERE id = $1', [id]);
+    res.json(allStudents.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 router.post('/students', async (req, res) => {
-  const { name, email, contact_no, date_of_birth } = req.body;
+  const { name, email, contact_no, date_of_birth, father_name, mother_name, ward_no, gender, photo_available, date_of_joining, date_of_leaving, active_status, registration_fees_paid_indicator, class_code } = req.body;
   try {
     const newStudent = await pool.query(
-      'INSERT INTO student (name, email, contact_no, date_of_birth) VALUES($1, $2, $3, $4) RETURNING *',
-      [name, email, contact_no, date_of_birth]
+      'INSERT INTO student (name, email, contact_no, date_of_birth, father_name, mother_name, ward_no, gender, photo_available, date_of_joining, date_of_leaving, active_status, registration_fees_paid_indicator, class_code) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *',
+      [name, email, contact_no, date_of_birth, father_name, mother_name, ward_no, gender, photo_available, date_of_joining, date_of_leaving, active_status, registration_fees_paid_indicator, class_code]
     );
     res.json(newStudent.rows[0]);
   } catch (err) {
